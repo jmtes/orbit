3b264df23b88740065b37cefd615f66fdad535f0
jmtes <juliennetesoro@gmail.com>
2021-06-23 11:40:50 -0700

    Add documentation

--- orbit-app/src/util/fetch.js
+++ orbit-app/src/util/fetch.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+// Fetch used for public endpoints of API
 const publicFetch = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export { publicFetch };
