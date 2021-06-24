17127fc5fdb36daade3550971f81690ddb7ce815
jmtes <juliennetesoro@gmail.com>
2021-06-24 16:29:04 -0700

    Persist auth state in local storage

--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -4,13 +4,28 @@ const AuthContext = createContext();
 const { Provider } = AuthContext;
 
 const AuthProvider = ({ children }) => {
-  const [authState, setAuthState] = useState({
-    token: null,
-    expiresAt: null,
-    userInfo: {},
+  const [authState, setAuthState] = useState(() => {
+    const token = localStorage.getItem('token') || null;
+    const expiresAt = localStorage.getItem('expiresAt') || null;
+    const userInfo = localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : {};
+
+    return {
+      token,
+      expiresAt,
+      userInfo,
+    };
   });
 
   const setAuthInfo = ({ token, expiresAt, userInfo }) => {
+    // Using local storage to persist auth state is frowned upon because LS is
+    // very susceptible to XSS. It's better to use HTTP-only cookies, but for
+    // now we're going with LS just to get things going.
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiresAt', expiresAt);
+    localStorage.setItem('userInfo', JSON.stringify(userInfo));
+
     setAuthState({ token, expiresAt, userInfo });
   };
 
