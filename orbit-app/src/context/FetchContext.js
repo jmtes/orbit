9912cacfaebb586c4c1098a44f47049f7d27b730
jmtes <juliennetesoro@gmail.com>
2021-06-28 13:23:40 -0700

    Include JWT in authenticated axios requests

--- orbit-app/src/context/FetchContext.js
+++ orbit-app/src/context/FetchContext.js
@@ -1,18 +1,29 @@
 import React, { createContext } from 'react';
 import axios from 'axios';
+import { useAuth } from './AuthContext';
 
 const FetchContext = createContext();
 const { Provider } = FetchContext;
 
 const FetchProvider = ({ children }) => {
+  const { authState } = useAuth();
+
   const authAxios = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL,
   });
+
+  authAxios.interceptors.request.use(
+    config => {
+      config.headers.Authorization = `Bearer ${authState.token}`;
+      return config;
+    },
+    error => Promise.reject(error)
+  );
 
   return (
     <Provider
       value={{
-        authAxios
+        authAxios,
       }}
     >
       {children}
