64039431848ad2f13f5c7e04e2010986925454bc
jmtes <juliennetesoro@gmail.com>
2021-06-28 23:34:43 -0700

    Remove interceptor

--- orbit-app/src/context/FetchContext.js
+++ orbit-app/src/context/FetchContext.js
@@ -1,24 +1,13 @@
 import React, { createContext } from 'react';
 import axios from 'axios';
-import { useAuth } from './AuthContext';
 
 const FetchContext = createContext();
 const { Provider } = FetchContext;
 
 const FetchProvider = ({ children }) => {
-  const { authState } = useAuth();
-
   const authAxios = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
   });
-
-  authAxios.interceptors.request.use(
-    config => {
-      config.headers.Authorization = `Bearer ${authState.token}`;
-      return config;
-    },
-    error => Promise.reject(error)
-  );
 
   return (
     <Provider
